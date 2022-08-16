e306fe0b7dd43021bf55feb048aa0f751c0e222f
VictoriaGerasimenko <gierasimienko1989@gmail.com>
2022-08-16 15:47:39 +0300

    added axios

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
+function displayCurrentTemperature(event) {
+  event.preventDefault();
+  let city = document.querySelector("#inputCity");
+  console.log(city.value);
+}
+let inputElement = document.querySelector("#search-form");
+inputElement.addEventListener("submit", displayCurrentTemperature);
 
+let apiUrl = "7ec50fc0a595f3c817fb74829d724ff2";
